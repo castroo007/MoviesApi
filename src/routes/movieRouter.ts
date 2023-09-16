9fbc42fbdcfd8093f7a23388ed88fd883af9b23c
Castro <joaocastro2013@hotmail.com>
2023-09-16 19:23:25 +0100

    second commit

--- src/routes/movieRouter.ts
+++ src/routes/movieRouter.ts
@@ -15,7 +15,7 @@ router.put("/movies/:id", MovieController.update);
 router.delete("/movies/:id", MovieController.delete);
 
 // Create a movie rating
-router.post("/ratings", MovieController.create);
+router.post("/ratings", MovieController.createRating);
 
 // Upload a movie poster image (only accessible to admin users)
 // router.post("/upload-poster", authenticateToken, MovieController.uploadPoster);
